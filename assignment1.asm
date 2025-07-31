45132b372e846e895e0ef7fd93669a90e2f79625
LadakaTayaara <82630115+LadakaTayaara@users.noreply.github.com>
2025-07-31 15:53:58 +0530

    Update assignment1.asm
    corrected the code

--- assignment1.asm
+++ assignment1.asm
@@ -7,22 +7,24 @@
 %endmacro
 
 section .data
-    msg db "The result is ", 0ah
+    msg db "The sum is: ", 0ah
+    msg_len equ $ - msg
     arr db 34H, 0AAH, 0FFH, 25H, 89H
-    temp dw 0
-    digit db 0
-    cnt db 4
+    arr_len equ $ - arr
+    
+    result_ascii db "0000", 0ah
+    result_ascii_len equ $ - result_ascii
 
 section .bss
 
 section .text
     global _start
 _start:
-    rw 1,1,msg,14
+    rw 1, 1, msg, msg_len
 
     mov ax, 00H
     mov rsi, arr
-    mov cl, 5
+    mov cl, arr_len
 
 nextno:
     add al, [rsi]
@@ -34,23 +36,28 @@ nocarry:
     dec cl
     jnz nextno
 
-again:
-    rol ax,4
-    mov [temp],ax
-    and ax,000FH
-    cmp al,09
-    jbe skip
-    add al,07H
+    mov rsi, result_ascii + 3
+    mov rcx, 4
 
-skip:
-    add al,30H
+convert_loop:
+    rol ax, 4
+    push rax
+    and al, 0FH
 
-    mov [digit],al
-    rw 1,1,digit,1
-    mov ax,[temp]
-    dec byte[cnt]
-    jnz again
+    cmp al, 09
+    jbe add_digit
+    add al, 7
+
+add_digit:
+    add al, 30h
+    mov [rsi], al
+
+    pop rax
+    dec rsi
+    loop convert_loop
+
+    rw 1, 1, result_ascii, result_ascii_len
 
     mov rax, 60
-    mov rdi, 0
-    syscall
+    mov rdi, 0
+    syscall
