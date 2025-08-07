488eff7468a344b4f3cc2949261327a5528efcff
LadakaTayaara <82630115+LadakaTayaara@users.noreply.github.com>
2025-08-07 16:17:31 +0530

    Update assignment2.asm
    corrected code

--- assignment2.asm
+++ assignment2.asm
@@ -1,67 +1,62 @@
 %macro rw 4
-    mov rax, %1
-    mov rdi, %2
-    mov rsi, %3
-    mov rdx, %4
-    syscall
+   mov rax, %1
+   mov rdi, %2
+   mov rsi, %3
+   mov rdx, %4
+   syscall
 %endmacro
 
 section .data
-	arr db 05h, 01h, 0ah, 25h, 45h
-	s1 db "sorting array is ", 20
-	s2 equ $-s1
+Arr db 05H, 01H, 0AH, 25H, 45H
+S1 db "Sorting array is", 20
+S2 equ $-S1
 
 section .bss
-	result resb 15
+
+result resb 15
 
 section .text
-	global _start
+global _start
 _start : 
-	mov bl, 4
-	
-nextp : mov cl, 4
-        mov rsi, arr
-        
-nextc : mov al, [rsi]
-        cmp al, [rsi + 1]
-        jbe noex 
-        xchg al, [rsi + 1]
-        mov [rsi], al
-        
-noex :
-        inc rsi
-        dec cl
-        
-	jnz nextc 
-	dec bl
-	jnz nextp
-	rw 1, 1, s1, s2
-	mov dl, 5
-	mov rdi, arr
-	mov rsi, result
-	
-nexteL: 
-	mov cl, 02
-        mov al, [rdi]
-        
-nextd : rol al, 4
-        mov bl, al
-        and al, 0fh
-        cmp al, 09h
-jbe down 
-        add al, 07h
-down :
-	add al, 30h
- 	mov [rsi], al 
- 	inc rsi
- 	mov al,bl
- 	dec cl
-	jnz nextd
-	;inc rsi
-	mov byte [rsi], 0ah
-	inc rsi
-	inc rdi
-	dec dl
-	jnz nexteL 
-	rw 1, 1, result, 15
-	rw 60, 0, 0, 0
+
+mov BL, 4
+NextP : mov CL, 4
+        mov rsi, Arr
+NextC : mov AL, [rsi]
+        CMP AL, [rsi + 1]
+        JAE NoEX 
+        XCHG AL, [rsi + 1]
+        MOV [rsi], AL
+NoEX :
+        INC rsi
+        DEC CL
+JNZ NextC 
+DEC BL
+JNZ NextP
+
+rw 1,1,S1,S2
+ mov DL, 5
+ mov rdi, Arr
+ mov rsi, result
+ NextEL: mov CL, 02
+          mov AL, [rdi]
+ NextD : ROL AL, 4
+         MOV BL, AL
+         AND AL, 0FH
+         CMP AL, 09H
+ JBE down 
+       Add AL, 07H
+ down :Add AL, 30H
+  mov [rsi], AL 
+  INC rsi
+  mov AL,BL
+  DEC CL
+  JNZ NextD
+  ;INC rsi
+  mov byte [rsi], 0AH
+  INC rsi
+  INC rdi
+  DEC DL
+  JNZ NextEL    
+  rw 1,1,result,15
+  rw 60,0,0,0
